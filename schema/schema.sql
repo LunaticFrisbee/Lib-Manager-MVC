3d66cfb16bef02381b93f4128c21511c3932b2d8
LunaticFrisbee <avnishchauhan065@gmail.com>
2022-07-26 23:03:48 +0530

    schema correctly populated

--- schema/schema.sql
+++ schema/schema.sql
@@ -2,12 +2,12 @@
 --
 -- Host: localhost    Database: project
 -- ------------------------------------------------------
--- Server version       8.0.29-0ubuntu0.22.04.2
+-- Server version	8.0.29-0ubuntu0.22.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -46,7 +46,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `books`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8*/;
 CREATE TABLE `books` (
   `title` varchar(100) NOT NULL,
   `quantity` bigint NOT NULL,
@@ -62,6 +62,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES ('Harry Potter',100,'1234',0),('Dan Brown\'s Inferno',50,'4321',0),('GoT',33,'5678',0);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +96,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8*/;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `salt` varchar(50) NOT NULL,
@@ -111,7 +112,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('avnish','29dc4ad8','123','f46c0a6df3f9301a2598701b604894e1cf1217e7d8da69c4bd5ad6c10263e22b'),('ace','66c835b6','2117032','fb84175fccfc481c8059ffe179ecf27540b1d63f0baf578bfa044f84f6c543ab');
+INSERT INTO `users` VALUES ('avnish','3f516438','21117032','05eae261c4a26c3952ad0d5afab16a3549deedff2d2fef2f7b9c153f6765146b');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -124,4 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-18  2:19:22
+-- Dump completed on 2022-07-26 22:13:53
